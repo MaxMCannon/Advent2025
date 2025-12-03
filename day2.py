38415aad15f52e9d67dcf1ead1a0aaec7d83fea5
Maxwell Cannon <maxwellcannon@maxwells-mbp.mynetworksettings.com>
2025-12-03 16:12:40 -0500

    moving devices

--- day2.py
+++ day2.py
@@ -30,18 +30,47 @@ print("Part 1: " + str(globalsum))
 
 #Start Part 2
 
-def checkid2(f, e):
-    for i in range(f, e + 1):
-        for i in range(1, len(i//2)):
-            pass
-        if len(str(i))%2 == 0:
-            p1 = str(i)[:int(len(str(i))/2)]
-            p2 = str(i)[int(len(str(i))/2):]
-            if p1 == p2:
-                doubleflag = False
-                for char in set(p1):
-                    if p1.count(char) == 2:
-                        doubleflag = True
-                if doubleflag:
-                    # print("Match found: " + str(i))
-                    outarr.append(i)
+def getarrlen(arr):
+    out = 0
+    for el in arr:
+        out += len(el)
+    return out
+
+def checknum(num):
+    # print(num)
+    for l in range(1, int(len(str(num))/2)+1):
+        arr = [str(num)[:l]]
+        print(arr)
+        for i in range(l, len(str(num))+1, l):
+            piece = str(num)[i:i+l]
+            # if piece == arr[-1]:
+            #     print(arr)
+            #     print("fail")
+            #     break
+            arr.append(piece)
+            print(arr)
+        if getarrlen(arr) == len(str(num)):
+            return num
+            print("got one")
+        else:
+            return 0
+    return 0
+
+p2sum = 0
+
+for l in input:
+    temp = []
+    ids = [int(l.split('-')[0]), int(l.split('-')[1])]
+    for i in range(ids[0], ids[1]):
+        print(i)
+        temp.append(checknum(i))
+    for item in temp:
+        p2sum += item
+    # print(ids)
+
+print(p2sum)
+
+# checknum(123123)
+        
+
+
